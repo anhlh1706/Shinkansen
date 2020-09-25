ab7ae45d1b805ffd5d89a62fa2a2e31b8fce3ea4
Hoang Anh <anh.le@fujitechjsc.com>
2020-09-25 08:29:01 +0700

    Initial Commit

---
+++ Shinkansen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shinkansen
+//
+//  Created by Lê Hoàng Anh on 25/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
